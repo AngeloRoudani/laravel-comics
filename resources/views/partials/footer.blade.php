37872b9e663a27833aceabd3d30fa801e516fff3
AngeloRoudani <ange.rouda@gmail.com>
2023-05-11 13:57:13 +0200

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+
+<h3>ciao</h3>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link"  href="#">Characters</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Comics</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled">Tv</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled">Games</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled">Collectibles</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled">Videos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+</header>
